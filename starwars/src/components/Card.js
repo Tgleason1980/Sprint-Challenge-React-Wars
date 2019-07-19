bf1d3e7cccbebb385a84d544a55200aeb8340fcb
Tgleason1980 <terryscoding@gmail.com>
2019-07-19 15:16:03 -0500

    MVP

--- starwars/src/components/Card.js
+++ starwars/src/components/Card.js
@@ -9,15 +9,21 @@ width: 250px;
 const Div = styled.div`
 display: flex;
 `
+const H2 = styled.h2`
+color: white;
+`
+const H3 = styled.h3`
+color: red;
+`
 const Card = (props)=>
 {
     return (
         <Div>
             <Section>
-            <h2>{props.person.name}</h2>
-            <h3>Gender:{props.person.gender}</h3>
-            <h3>Hair Color:{props.person.hair_color}</h3>
-            <h3>Skin Color:{props.person.skin_color}</h3>
+            <H2>{props.person.name}</H2>
+            <H3>Gender: {props.person.gender}</H3>
+            <H3>Hair Color: {props.person.hair_color}</H3>
+            <H3>Skin Color: {props.person.skin_color}</H3>
             </Section>
         </Div>
     )
